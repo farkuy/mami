9a08e3b35f2dbe414798ea8dfc4b927a119ceecb
farkuy <darkuy@mail.ru>
2026-05-18 17:15:08 +0300

    fix style

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const githubPagesPath = isGithubActions && repositoryName ? `/${repositoryName}` : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: githubPagesPath,
+  assetPrefix: githubPagesPath,
   images: {
     unoptimized: true,
   },
