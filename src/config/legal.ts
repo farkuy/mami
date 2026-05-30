1fa6a358fcab978018211ec829771cc509ac6542
farkuy <darkuy@mail.ru>
2026-05-30 22:18:14 +0300

    1 stage

---
+++ src/config/legal.ts
@@ -0,0 +1,12 @@
+export const operatorName = 'Богдашова Ирина Александровна';
+
+export const operatorStatus = 'самозанятая';
+
+export const operatorInn = '524928281904';
+
+export const siteDomain = 'SITE_DOMAIN_PLACEHOLDER';
+
+export const policyUpdatedAt = '2026-05-30';
+
+export const personalDataConsentVersion = 'personal-data-consent-v1';
+
